c827d05d6de178df2766e2a6956a18486bf847b0
Randy Leighton <randyleighton@yahoo.com>
2014-11-20 22:16:37 -0800

    angular module and controllers in place

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module('menu', ['ui.bootstrap']);
+
+app.controller('MainCtrl', [function($scope){
+  $scope.name = 'Nacho craver';
+ 
+}])
+
+app.controller('DropdownCtrl', [function($scope){
+  $scope.items = [
+    "Chooose Chips",
+    "Choose Beans",
+    "Choose Nachos"
+  ];
+}]);
